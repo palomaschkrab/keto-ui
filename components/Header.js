a8e32fce21a9df298802ca25e0c55480c0bb1adb
Paloma Schkrab <paloma.schkrab@gmail.com>
2019-05-08 22:20:06 -0700

    TG-4: Added bootstrap to UI

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Head from 'next/head'
 
 const linkStyle = {
   marginRight: 15
@@ -6,6 +7,10 @@ const linkStyle = {
 
 const Header = () => (
   <div>
+    <Head>
+      <title>How to Keto Cook</title>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
+    </Head>
     <Link href="/">
       <a style={linkStyle}>Home</a>
     </Link>
