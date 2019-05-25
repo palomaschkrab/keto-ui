feb2153b592de8bd752d10d5e9d7f2d30f097fe4
Paloma Schkrab <paloma.schkrab@gmail.com>
2019-05-25 14:09:24 -0700

    TG-9:Finished editing header

--- components/Header.js
+++ components/Header.js
@@ -9,7 +9,12 @@ const Header = () => (
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
       </Head>
       <nav className="navbar navbar-expand-lg navbar-light">
-        <a className="navbar-brand" href="#">How to Keto Cook</a>
+        <Link href="/">
+            <a>
+              <img src="../static/images/logo.png" className='navbar-brand' alt='logo' width="150px" height="65px"/>
+            </a>                
+        </Link>
+        {/* <a className="navbar-brand" href="#">How to Keto Cook</a> */}
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -17,7 +22,7 @@ const Header = () => (
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
             </li>
           </ul>
         </div>
@@ -28,6 +33,11 @@ const Header = () => (
       #header {
         background-color: #DDA0DD;
       } 
+      li {
+        font-size: 160%;
+        font-style: italic;
+        font-family: "Times New Roman", Times, serif;
+      }
 
     `}</style>
   </div>
